52cb34aa761b999a21e6ffcf92388a88605e23ea
EndorphinSponge <hqiu1@ualberta.ca>
2022-08-20 21:03:59 -0400

    Refactored abrvs into its own module

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ DIRPATH = os.path.dirname(os.path.abspath(__file__))
 os.chdir(DIRPATH)
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-if 1:
+if 0:
     from models_spacy import SpacyExtractor
     a = SpacyExtractor()
     a.addPipeSampleSize()
@@ -16,56 +16,22 @@ if 1:
 
 
 
-if 0: # Temporary pipeline without GPT3 component 
+
+if 1: # Full pipeline example using test.xlsx
     ROOT_PATH = "data/test/test.xlsx"
     MODEL = "gpt3"
     THRESH = 2
     ROOT_NAME = os.path.splitext(ROOT_PATH)[0]
+    RUN_CLOUD = 0 # Extra guard against running cloud model
     
-
-    from models_spacy import PostProcessor, refineAbrvs
-    localmodel = PostProcessor()
-    localmodel.extractEnts(f"{ROOT_NAME}_{MODEL}F.xlsx") # *_gpt3F_entsR.xlsx
-    localmodel.extractAbrvCont(ROOT_PATH) # *_abrvs.json
-    print(localmodel.empty_log)
-    refineAbrvs(f"{ROOT_NAME}_abrvs.json") # *_abrvs_rfn.json, *_abrvs_trans.json
+    if RUN_CLOUD:
+        from models_api import CloudModel
+        cloudmodel = CloudModel(ROOT_PATH)
+        cloudmodel.mineTextGpt3() # *_gpt3R.xlsx
+        cloudmodel.exportOutputFormatted(MODEL) # *_gpt3F.xlsx
     
-    from graph_builder import EntProcessor
-    from components_diseases import TBICore
-    core = TBICore(abrv_path=f"{ROOT_NAME}_abrvs_rfn.json",
-                   common_trans_path=f"{ROOT_NAME}_abrvs_trans.json")
-    processor = EntProcessor(ent_processor_core=core)
-    processor.procDfEnts(f"{ROOT_NAME}_{MODEL}F_entsR.xlsx") # *_gpt3F_entsF.xlsx
-    processor.printLogs()
-    
-    from graph_builder import GraphBuilder
-    builder = GraphBuilder()
-    builder.popCountersMulti(f"{ROOT_NAME}_{MODEL}F_entsF.xlsx")
-    builder.buildGraph(thresh=THRESH, multidi=True)
-    builder.exportGraph() # *_gpt3F_entsF_t{int}.xml
-    
-    from graph_renderer import GraphVisualizer
-    visualizer = GraphVisualizer(f"{ROOT_NAME}_{MODEL}F_entsF_t{THRESH}.xml")
-    visualizer.renderBarGraph(ent_types=["factor", "outcome"])
-    visualizer.genRenderArgs()
-    visualizer.genLegend()
-    visualizer.renderGraphNX() # *_gpt3_t{int}_net(<rendering info>).png
-    # visualizer.renderGraphPyvis() # *_gpt3_t{int}_pyvis.html
-
-F = False # To guard against running of test code while still keeping linting active
-
-if F: # Full pipeline example using test.xlsx
-    ROOT_PATH = "data/test/test.xlsx"
-    MODEL = "gpt3"
-    THRESH = 2
-    ROOT_NAME = os.path.splitext(ROOT_PATH)[0]
-    
-    from models_api import CloudModel
-    cloudmodel = CloudModel(ROOT_PATH)
-    cloudmodel.mineTextGpt3() # *_gpt3R.xlsx
-    cloudmodel.exportOutputFormatted(MODEL) # *_gpt3F.xlsx
-    
-    from models_spacy import PostProcessor, refineAbrvs
+    from models_spacy import PostProcessor
+    from abrvs_syns import refineAbrvs
     localmodel = PostProcessor()
     localmodel.extractEnts(f"{ROOT_NAME}_{MODEL}F.xlsx") # *_gpt3F_entsR.xlsx
     localmodel.extractAbrvCont(ROOT_PATH) # *_abrvs.json
@@ -94,8 +60,8 @@ if F: # Full pipeline example using test.xlsx
     visualizer.renderGraphNX() # *_gpt3_t{int}_net(<rendering info>).png
     visualizer.renderGraphPyvis() # *_gpt3_t{int}_pyvis.html
     
-    
-if F: # Pipeline to render graphs for each topic 
+# 0/1 guard to keep linting active without running code
+if 0: # Pipeline to render graphs for each topic 
     ROOT_PATH = "data/gpt3_output.xlsx"
     SUFFIX = "_topics"
     MODEL = "gpt3"
